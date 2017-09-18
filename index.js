b6aba2b38d4447cb47a854b6591dd9ce6f952e68
otomchuk <olenka.tomchuk@gmail.com>
2017-09-18 22:03:35 +0300

    [refactoring] According to Eslint rules

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const TelegramBot = require('node-telegram-bot-api');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const TOKEN = process.env.TELEGRAM_BOT_API_TOKEN;
 
-const options = {
+var options = {
   webHook: {
     port: process.env.PORT
   }
 };
 
-const url = process.env.APP_URL || 'https://telegram-bot-clean-ukraine.herokuapp.com:443';
-const bot = new TelegramBot(TOKEN, options);
+var url = process.env.APP_URL || 'https://telegram-bot-clean-ukraine.herokuapp.com:443';
+var bot = new TelegramBot(TOKEN, options);
 
-bot.setWebHook(`${url}/bot${TOKEN}`);
+bot.setWebHook(url + '/bot' + TOKEN);
 
-bot.onText(/^\/start/, function(msg, match) {
+bot.onText(/^\/start/, function(msg) {
   var option = {
-    "parse_mode": "Markdown",
-    "reply_markup": {
-      "one_time_keyboard": true,
-      "keyboard": [
+    'parse_mode': 'Markdown',
+    'reply_markup': {
+      'one_time_keyboard': true,
+      'keyboard': [
         [
           {
-            text: "Моє розташування",
+            text: 'Моє розташування',
             request_location: true
           }
         ],
-        ["Відмова"]
+        ['Відмова']
       ]
     }
   };
 
-  bot.sendMessage(msg.chat.id, "Де ви знаходитесь?", option).then(() => {
-    bot.once("location", (msg) => {
-      bot.sendMessage(msg.chat.id, "Дякую, що поділилися вашим місцем знаходження." + [msg.location.longitude, msg.location.latitude].join(";"));
+  bot.sendMessage(msg.chat.id, 'Де ви знаходитесь?', option).then(function() {
+    bot.once('location', function(msg) {
+      bot.sendMessage(msg.chat.id, 'Дякую, що поділилися вашим місцем знаходження.' + [msg.location.longitude, msg.location.latitude].join(';'));
     });
   });
 });
