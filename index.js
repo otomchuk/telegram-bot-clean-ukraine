eb466b69d95ca2f89b2ba445ddc2945119dce794
otomchuk <olenka.tomchuk@gmail.com>
2017-09-18 21:09:29 +0300

    [start] request_location

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ const bot = new TelegramBot(TOKEN, options);
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+bot.onText(/^\/start/, function(msg, match) {
+  var option = {
+    "parse_mode": "Markdown",
+    "reply_markup": {
+      "one_time_keyboard": true,
+      "keyboard": [
+        [
+          {
+            text: "Моє розташування",
+            request_location: true
+          }
+        ],
+        ["Відмова"]
+      ]
+    }
+  };
+
+  bot.sendMessage(msg.chat.id, "Де ви знаходитесь?", option).then(() => {
+    bot.once("location", (msg) => {
+      bot.sendMessage(msg.chat.id, "Дякую, що поділилися вашим місцем знаходження." + [msg.location.longitude, msg.location.latitude].join(";"));
+    });
+  });
 });
