8eb34eae8d424a9e04e1cc208be0a36c5683c5ec
otomchuk <olenka.tomchuk@gmail.com>
2017-09-19 22:29:05 +0300

    [refactoring] Devide code for step functions

--- index.js
+++ index.js
@@ -18,6 +18,29 @@ bot.setWebHook(url + '/bot' + TOKEN);
 
 // On command '/start'
 bot.onText(/^\/start/, function(msg) {
+  stepOneGetUserLocation(msg).then(function() {
+    bot.once('location', function(msg) {
+
+      var userLocation = {
+        lat: msg.location.latitude,
+        lng: msg.location.longitude
+      };
+
+      stepTwoChooseRawType(msg).then(function() {
+        bot.on('callback_query', function(rawType) {
+
+          sendStepTwoResponce(msg, rawType.data, userLocation);
+
+          setTimeout(function() {
+            stepThreeChooseWhatToDo(msg);
+          }, 1000);
+        });
+      });
+    });
+  });
+});
+
+function stepOneGetUserLocation(msg) {
   // Options for displaying keyboard that asks users to share location
   var option = {
     'parse_mode': 'Markdown',
@@ -31,44 +54,55 @@ bot.onText(/^\/start/, function(msg) {
   };
 
   // Step-1: where are you located
-  bot.sendMessage(msg.chat.id, 'Де ви знаходитесь?', option).then(function() {
-    bot.once('location', function(msg) {
+  return bot.sendMessage(msg.chat.id, 'Де ви знаходитесь?', option);
+}
 
-      var userLocation = {
-        lat: msg.location.latitude,
-        lng: msg.location.longitude
-      };
+function stepTwoChooseRawType(msg) {
+  // Options for displaying keyboard that asks user to choose raw types
+  var options = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Папір', callback_data: 'paper' },
+          { text: 'Скло', callback_data: 'glass' }
+        ],
+        [
+          { text: 'Пластик', callback_data: 'plastic' },
+          { text: 'Метал', callback_data: 'metal' }
+        ],
+      ]
+    }
+  };
 
-      // Options for displaying keyboard that asks user to choose raw types
-      var options = {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: 'Папір', callback_data: 'paper' },
-              { text: 'Скло', callback_data: 'glass' }
-            ],
-            [
-              { text: 'Пластик', callback_data: 'plastic' },
-              { text: 'Метал', callback_data: 'metal' }
-            ],
-          ]
-        }
-      };
+  // Step-2: choose raw material that you would like to give for recycling
+  return bot.sendMessage(msg.chat.id, 'Оберіть тип сировини, який ви хочете здати?', options);
+}
 
-      // Step-2: choose raw material that you would like to give for recycling
-      bot.sendMessage(msg.chat.id, 'Оберіть тип сировини, який ви хочете здати?', options).then(function() {
-        bot.on('callback_query', function(rawType) {
+// Step-2: responce with closes recycling point
+function sendStepTwoResponce(msg, rawType, userLocation) {
+  var recyclingPoints = DB.getRecyclingPointsFor(rawType);
+  var closesRecyclingPoint = utils.findClosestLocation(recyclingPoints, userLocation.lat, userLocation.lng);
 
-          var recyclingPoints = DB.getRecyclingPointsFor(rawType.data);
-          var closesRecyclingPoint = utils.findClosestLocation(recyclingPoints, userLocation.lat, userLocation.lng);
+  // Step-2: Responce with closes recycing point
+  bot.sendLocation(msg.chat.id, closesRecyclingPoint.lat, closesRecyclingPoint.lng);
 
-          // Step-2: Responce with closes recycing point
-          bot.sendLocation(msg.chat.id, closesRecyclingPoint.lat, closesRecyclingPoint.lng);
-          bot.sendMessage(msg.chat.id, 'Найближчий пункт для прийому: ' + closesRecyclingPoint.description);
+  setTimeout(function() {
+    bot.sendMessage(msg.chat.id, 'Найближчий пункт для прийому: ' + closesRecyclingPoint.description);
+  }, 500);
+}
 
-          // TODO: Step-3: Show all | New search | End
-        });
-      });
-    });
-  });
-});
+function stepThreeChooseWhatToDo(msg) {
+  // Options for displaying keyboard that asks about next steps
+  var option = {
+    'parse_mode': 'Markdown',
+    'reply_markup': {
+      'one_time_keyboard': true,
+      'keyboard': [
+        ['Покажи список найближчих пунктів'],
+        ['Новий пошук', 'Завершити роботу']
+      ]
+    }
+  };
+
+  return bot.sendMessage(msg.chat.id, 'Вам підходить цей пункт прийому?', option);
+}
